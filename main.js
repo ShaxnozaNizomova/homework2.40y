69c9ab75320e6617a100cc3a27766092b6f9c153
ShaxnozaNizomova <xurshidatosheva1918@gmail.com>
2023-10-09 21:14:15 +0500

    homework

---
+++ main.js
@@ -0,0 +1,22 @@
+// 1 masala
+
+let b= 30;
+
+if (b % 2 === 0) {
+  console.log("juft");
+} else {
+ console.log("Juft emas");
+}
+
+// 2-masala
+
+let a = Math.floor(Math.random()*100); 
+
+ console.log("Random son: " + a);
+
+if (a % 5 === 0) {
+console.log("Son 5 ga bolinadi"); } else if (a % 3 === 0) {
+console.log("Son 3 ga bolinadi"); } else if (a % 3 === 0 && a % 5 === 0) {
+console.log("Son 3 va 5 ga bolinadi");} else {
+console.log("Son 3 va 5 ga bolinmaydi"); }
+
